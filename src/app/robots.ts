e28a4d456bbddb4d45bed27f71e15ddc95f2e2b1
Pervez14 <ecomright.pk@gmail.com>
2026-07-27 13:22:14 +0500

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next';
 
-const baseUrl = 'https://mbnpakistan.com';
+const baseUrl = 'https://www.mbnpakistan.com';
 
 export default function robots(): MetadataRoute.Robots {
   return {
